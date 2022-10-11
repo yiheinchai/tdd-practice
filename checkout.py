bb7d41b64ba2d78fdb33c62d28cd556ea23d060f
Yi Hein Chai <chaiyihein@gmail.com>
2022-10-11 12:55:00 +0100

    Added discount rules

--- checkout.py
+++ checkout.py
@@ -1,10 +1,15 @@
 class Checkout:
     def __init__(self) -> None:
         self._items = []
+        self._discount_rules = []
 
     @property
     def items(self):
         return self._items
+
+    @property
+    def discount_rules(self):
+        return self._discount_rules
 
     @property
     def total_cost(self):
@@ -18,6 +23,10 @@ class Checkout:
     def items(self, new_items):
         self._items = new_items
 
+    @discount_rules.setter
+    def discount_rules(self, new_rules):
+        self._discount_rules = new_rules
+
     def _find_item(self, item_name):
         for item in self.items:
             if item.name == item_name:
@@ -26,14 +35,37 @@ class Checkout:
     def add(self, item):
         itm = self._find_item(item.name)
         if itm:
-            itm.qty += 1
+            itm.qty += item.qty
         else:
             self.items = [*self.items, item]
 
     def apply_discount(self):
-        for item in self.items:
-            if item.qty > 2:
-                item.price = 1
+        for rule in self.discount_rules:
+            itm = self._find_item(rule.item_name)
+            if itm and itm.qty >= rule.qty_req:
+                itm.price = itm.price * rule.discount
+
+    def add_discount_rule(self, discount_rule):
+        self.discount_rules = [*self.discount_rules, discount_rule]
+
+
+class DiscountRule:
+    def __init__(self, item_name, qty_req, discount):
+        self._item_name = item_name
+        self._qty_req = qty_req
+        self._discount = discount
+
+    @property
+    def item_name(self):
+        return self._item_name
+
+    @property
+    def qty_req(self):
+        return self._qty_req
+
+    @property
+    def discount(self):
+        return self._discount
 
 
 class Item:
--- test_checkout.py
+++ test_checkout.py
@@ -1,15 +1,29 @@
-from checkout import Checkout, Item
+from checkout import Checkout, Item, DiscountRule
 import pytest
 
 
 @pytest.fixture
-def cart():
+def coffeepack():
+    return Item('Coffee', 3, 4)
+
+
+@pytest.fixture
+def discountrule():
+    return DiscountRule('Coffee', 3, 0.5)
+
+
+@pytest.fixture
+def cart(coffeepack):
     ckout = Checkout()
-    ckout.add(Item('Coffee', 3))
-    ckout.add(Item('Coffee', 3))
-    ckout.add(Item('Coffee', 3))
+    ckout.add(coffeepack)
     ckout.add(Item('Tea', 2))
     return ckout
+
+
+@pytest.fixture
+def cart_with_discount(cart, discountrule):
+    cart.add_discount_rule(discountrule)
+    return cart
 
 
 class TestItem:
@@ -38,6 +52,14 @@ class TestItem:
         assert itm.price == 3
 
 
+class TestDiscountRule:
+    def test_discount_attrs(self):
+        dr = DiscountRule('Coffee', 3, 0.50)
+        assert dr.item_name == 'Coffee'
+        assert dr.qty_req == 3
+        assert dr.discount == 0.50
+
+
 class TestCheckout:
     def test_initial_empty_checkout_list(self):
         ckout = Checkout()
@@ -63,8 +85,12 @@ class TestCheckout:
         assert len(ckout.items) == 2
 
     def test_get_total_cost(self, cart):
-        assert cart.total_cost == 11
+        assert cart.total_cost == 14
 
-    def test_apply_discount(self, cart):
-        cart.apply_discount()
-        assert cart.total_cost == 5
+    def test_add_discount_rule(self, cart):
+        cart.add_discount_rule(DiscountRule('Coffee', 3, 0.5))
+        assert cart.discount_rules[0].item_name == 'Coffee'
+
+    def test_apply_discount(self, cart_with_discount):
+        cart_with_discount.apply_discount()
+        assert cart_with_discount.total_cost == 8
